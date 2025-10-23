6607b89ab5dd3cd000fa9a3777796e6e623ca8a6
José Garcia <70547153+ojosegarcia@users.noreply.github.com>
2025-10-23 15:45:50 -0300

    docs: Crianção do README

---
+++ README.md
@@ -0,0 +1,16 @@
+# Estuda Mais
+Uma aplicação full-stack de agendamento de aulas particulares utilizando Spring Boot e Angular.
+
+## Prévia da aplicação
+
+## Tecnologias
+**Backend**
+- Java 21
+- PostgreSQL
+- H2 Database
+- Maven
+- Flayway
+- Lobok
+
+
+## Como Rodar
--- src/main/java/com/fatec/estudamaisbackend/controllers/UsuarioController.java
+++ src/main/java/com/fatec/estudamaisbackend/controllers/UsuarioController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -25,10 +26,42 @@ public class UsuarioController {
                 return ResponseEntity.status(HttpStatus.OK).body(listUsuarios);
     }
 
+
+    @GetMapping("/{id}")
+        public ResponseEntity getById(@PathVariable(value = "id") Integer id){
+        Optional usuario = repository.findById(id);
+      if (usuario.isEmpty()){
+          return ResponseEntity.status(HttpStatus.FOUND).body("User not found");
+      }
+        return ResponseEntity.status(HttpStatus.FOUND).body(usuario.get());
+    }
+
+
     @PostMapping
     public ResponseEntity  save(@RequestBody UsuarioDto dto){
         var usuario = new Usuario();
         BeanUtils.copyProperties(dto,usuario);
         return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(usuario));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable(value = "id") Integer id){
+        Optional<Usuario> usuario = repository.findById(id);
+        if (usuario.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+        repository.delete(usuario.get());
+        return ResponseEntity.status(HttpStatus.OK).body("User deleted");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity update(@PathVariable(value = "id") Integer id, @RequestBody UsuarioDto dto){
+        Optional<Usuario> usuario = repository.findById(id);
+        if (usuario.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+        var usuarioModel = usuario.get();
+        BeanUtils.copyProperties(dto,usuarioModel);
+        return ResponseEntity.status(HttpStatus.OK).body(repository.save(usuarioModel));
     }
 }
